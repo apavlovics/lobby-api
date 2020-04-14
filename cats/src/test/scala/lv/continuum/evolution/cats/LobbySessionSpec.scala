5b07de99c238b7c71ef276d097c5e6f0a0baf1cd
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-04-14 15:12:03 +0300

    Continue working on LobbySessionSpec

--- cats/src/test/scala/lv/continuum/evolution/cats/LobbySessionSpec.scala
+++ cats/src/test/scala/lv/continuum/evolution/cats/LobbySessionSpec.scala
@@ -1,7 +1,7 @@
 package lv.continuum.evolution.cats
 
-import cats.effect.concurrent.Ref
 import cats.effect.IO
+import cats.effect.concurrent.Ref
 import cats.implicits._
 import fs2.concurrent.Queue
 import lv.continuum.evolution.auth.{Authenticator => CommonAuthenticator}
@@ -22,27 +22,34 @@ class LobbySessionSpec
 
   private implicit val limit: Duration = 30.seconds
 
-  private val lobbySessionIO: IO[LobbySession[IO]] = for {
-    lobbyRef <- Ref.of[IO, Lobby](Lobby())
-    subscribersRef <- Ref.of[IO, Subscribers[IO]](Set.empty)
-    sessionParamsRef <- Ref.of[IO, SessionParams](SessionParams())
-    subscriber <- Queue.unbounded[IO, PushOut]
-    lobbySession = LobbySession[IO](
-      // TODO Replace with mock implementation
-      authenticator = Authenticator[IO](new CommonAuthenticator),
-      lobbyRef = lobbyRef,
-      subscribersRef = subscribersRef,
-      sessionParamsRef = sessionParamsRef,
-      subscriber = subscriber,
-    )
-  } yield lobbySession
+  private val notAuthenticatedAuthenticator: Authenticator[IO] = {
+    val commonAuthenticator = new CommonAuthenticator {
+      override def authenticate(username: Username, password: Password): Option[UserType] = None
+    }
+    Authenticator[IO](commonAuthenticator)
+  }
+
+  private def lobbySessionIO(authenticator: Authenticator[IO]): IO[LobbySession[IO]] =
+    for {
+      lobbyRef <- Ref.of[IO, Lobby](Lobby())
+      subscribersRef <- Ref.of[IO, Subscribers[IO]](Set.empty)
+      sessionParamsRef <- Ref.of[IO, SessionParams](SessionParams())
+      subscriber <- Queue.unbounded[IO, PushOut]
+      lobbySession = LobbySession[IO](
+        authenticator = authenticator,
+        lobbyRef = lobbyRef,
+        subscribersRef = subscribersRef,
+        sessionParamsRef = sessionParamsRef,
+        subscriber = subscriber,
+      )
+    } yield lobbySession
 
   "LobbySession" when {
 
     "not authenticated" should {
       "decline authentication upon invalid credentials" in run {
         for {
-          lobbySession <- lobbySessionIO
+          lobbySession <- lobbySessionIO(notAuthenticatedAuthenticator)
           out <- lobbySession.process(Login(Username("test"), Password("test")).asRight)
         } yield out should contain(loginFailed._2)
       }
--- common/src/main/scala/lv/continuum/evolution/auth/Authenticator.scala
+++ common/src/main/scala/lv/continuum/evolution/auth/Authenticator.scala
@@ -5,7 +5,7 @@ import lv.continuum.evolution.protocol.Protocol.UserType._
 import lv.continuum.evolution.protocol.Protocol._
 
 /** A simple in-memory `Authenticator` service. While it is enough for the purposes of this sample
-  * application, a real-world alternative would probably need to work asynchronously. */
+  * application, a real-world implementation would probably need to work asynchronously. */
 class Authenticator {
 
   def authenticate(username: Username, password: Password): Option[UserType] =
