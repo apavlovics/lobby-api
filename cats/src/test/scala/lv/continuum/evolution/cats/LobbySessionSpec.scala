db9cfa6e9967d919bd16fc6b7a50c7783ca79fa1
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-04-15 09:07:29 +0300

    Continue working on LobbySessionSpec

--- cats/src/test/scala/lv/continuum/evolution/cats/LobbySessionSpec.scala
+++ cats/src/test/scala/lv/continuum/evolution/cats/LobbySessionSpec.scala
@@ -6,6 +6,7 @@ import cats.implicits._
 import fs2.concurrent.Queue
 import lv.continuum.evolution.auth.{Authenticator => CommonAuthenticator}
 import lv.continuum.evolution.model.Lobby
+import lv.continuum.evolution.protocol.Protocol.UserType._
 import lv.continuum.evolution.protocol.Protocol._
 import lv.continuum.evolution.protocol.TestData
 import org.scalatest.matchers.should.Matchers
@@ -21,11 +22,11 @@ class LobbySessionSpec
 
   private implicit val limit: Duration = 5.seconds
 
-  private val notAuthenticatedAuthenticator = {
-    val mock = new CommonAuthenticator {
-      override def authenticate(username: Username, password: Password): Option[UserType] = None
+  private def stubAuthenticator(userType: Option[UserType]): Authenticator[IO] = {
+    val stub = new CommonAuthenticator {
+      override def authenticate(username: Username, password: Password): Option[UserType] = userType
     }
-    Authenticator[IO](mock)
+    Authenticator[IO](stub)
   }
 
   private def lobbySessionIO(authenticator: Authenticator[IO]): IO[LobbySession[IO]] =
@@ -46,33 +47,61 @@ class LobbySessionSpec
   "LobbySession" when {
 
     "not authenticated" should {
+
+      val notAuthenticatedLobbySessionIO = lobbySessionIO(stubAuthenticator(None))
+
       "decline authentication upon invalid credentials" in run {
         for {
-          lobbySession <- lobbySessionIO(notAuthenticatedAuthenticator)
+          lobbySession <- notAuthenticatedLobbySessionIO
           out <- lobbySession.process(login._2.asRight)
         } yield out should contain(loginFailed._2)
       }
       "decline responding to pings" in run {
         for {
-          lobbySession <- lobbySessionIO(notAuthenticatedAuthenticator)
+          lobbySession <- notAuthenticatedLobbySessionIO
           out <- lobbySession.process(ping._2.asRight)
         } yield out should contain(notAuthenticated._2)
       }
       "decline processing TableIn messages" in run {
         for {
-          lobbySession <- lobbySessionIO(notAuthenticatedAuthenticator)
+          lobbySession <- notAuthenticatedLobbySessionIO
           out <- lobbySession.process(subscribeTables._2.asRight)
         } yield out should contain(notAuthenticated._2)
       }
       "decline processing AdminTableIn messages" in run {
         for {
-          lobbySession <- lobbySessionIO(notAuthenticatedAuthenticator)
+          lobbySession <- notAuthenticatedLobbySessionIO
           out <- lobbySession.process(addTable._2.asRight)
         } yield out should contain(notAuthenticated._2)
       }
       "report invalid messages" in run {
         for {
-          lobbySession <- lobbySessionIO(notAuthenticatedAuthenticator)
+          lobbySession <- notAuthenticatedLobbySessionIO
+          out <- lobbySession.process(error.asLeft)
+        } yield out should contain(invalidMessage._2)
+      }
+    }
+
+    "authenticated as User" should {
+
+      val userLobbySessionIO = for {
+        lobbySession <- lobbySessionIO(stubAuthenticator(User.some))
+        out <- lobbySession.process(login._2.asRight)
+        _ <- IO {
+          out should contain(loginSuccessfulUser._2)
+        }
+      } yield lobbySession
+
+      "respond to pings" in run {
+        for {
+          lobbySession <- userLobbySessionIO
+          out <- lobbySession.process(ping._2.asRight)
+        } yield out should contain(pong._2)
+      }
+      // TODO Complete implementation
+      "report invalid messages" in run {
+        for {
+          lobbySession <- userLobbySessionIO
           out <- lobbySession.process(error.asLeft)
         } yield out should contain(invalidMessage._2)
       }
