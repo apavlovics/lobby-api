39e0b554a96c5d5ffef96f9372b8ccfd3bacc82e
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-08-17 23:09:13 +0300

    Improve JavaScript code structure

--- client/main.js
+++ client/main.js
@@ -1,12 +1,18 @@
-const outgoingMessageTypeSelect = $("#outgoing-message-type")
-const outgoingMessageTextArea = $("#outgoing-message")
-const incomingMessagesList = $("#incoming-messages")
+$(document).ready(() => {
+  const select = $('#outgoing-message-type')
+  const textArea = $('#outgoing-message')
+  const list = $('#incoming-messages')
 
-function updateOutgoingMessage() {
-  const outgoingMessageType = outgoingMessageTypeSelect.children("option:selected").val()
+  select.change(() => updateOutgoingMessage(select, textArea))
+  updateOutgoingMessage(select, textArea)
+  openWebSocket(textArea, list)
+})
+
+function updateOutgoingMessage(select, textArea) {
+  const outgoingMessageType = select.children('option:selected').val()
   let message
   switch(outgoingMessageType) {
-    case "login":
+    case 'login':
       message =
 `{
   "$type": "login",
@@ -14,26 +20,26 @@ function updateOutgoingMessage() {
   "password": "admin"
 }`
       break
-    case "ping":
+    case 'ping':
       message =
 `{
   "$type": "ping",
   "seq": 12345
 }`
       break
-    case "subscribe_tables":
+    case 'subscribe_tables':
       message =
 `{
   "$type": "subscribe_tables"
 }`
       break
-    case "unsubscribe_tables":
+    case 'unsubscribe_tables':
       message =
 `{
   "$type": "unsubscribe_tables"
 }`
       break
-    case "add_table":
+    case 'add_table':
       message =
 `{
   "$type": "add_table",
@@ -44,7 +50,7 @@ function updateOutgoingMessage() {
   }
 }`
       break
-    case "update_table":
+    case 'update_table':
       message =
 `{
   "$type": "update_table",
@@ -55,7 +61,7 @@ function updateOutgoingMessage() {
   }
 }`
       break
-    case "remove_table":
+    case 'remove_table':
       message =
 `{
   "$type": "remove_table",
@@ -66,38 +72,38 @@ function updateOutgoingMessage() {
       console.log(`Outgoing message type ${outgoingMessageType} is invalid`)
       break
   }
-  outgoingMessageTextArea.val(message)
+  textArea.val(message)
 }
-outgoingMessageTypeSelect.change(updateOutgoingMessage)
-$(updateOutgoingMessage)
 
-const webSocket = new WebSocket("ws://localhost:9000/lobby_api")
-webSocket.onopen = () => {
-  const sendButton = $("#send")
-  sendButton.removeAttr("disabled")
-  sendButton.click(() => {
-    const outgoingMessageFormat = $("input[name=outgoing-message-format]:checked").val()
-    switch (outgoingMessageFormat) {
-      case "text":
-        webSocket.send(outgoingMessageTextArea.val())
-        break
-      case "binary":
-        webSocket.send(new Blob([outgoingMessageTextArea.val()]))
-        break
-      default:
-        console.log(`Outgoing message format ${outgoingMessageFormat} is invalid`)
-        break
+function openWebSocket(textArea, list) {
+  const webSocket = new WebSocket('ws://localhost:9000/lobby_api')
+  webSocket.onopen = () => {
+    const sendButton = $('#send')
+    sendButton.removeAttr('disabled')
+    sendButton.click(() => {
+      const outgoingMessageFormat = $('input[name=outgoing-message-format]:checked').val()
+      switch (outgoingMessageFormat) {
+        case 'text':
+          webSocket.send(textArea.val())
+          break
+        case 'binary':
+          webSocket.send(new Blob([textArea.val()]))
+          break
+        default:
+          console.log(`Outgoing message format ${outgoingMessageFormat} is invalid`)
+          break
+      }
+    })
+    list.prepend($('<li>Connected via WebSocket</li>'))
+  }
+  webSocket.onerror = error => {
+    console.log('Issue with WebSocket connection', error)
+  }
+  webSocket.onmessage = event => {
+    if (typeof event.data === 'string') {
+      list.prepend($(`<li>${event.data}</li>`))
+    } else {
+      console.log(`Data ${event.data} is not supported`)
     }
-  })
-  incomingMessagesList.prepend($("<li>Connected via WebSocket</li>"))
-}
-webSocket.onerror = error => {
-  console.log("Issue with WebSocket connection", error)
-}
-webSocket.onmessage = event => {
-  if (typeof event.data === "string") {
-    incomingMessagesList.prepend($(`<li>${event.data}</li>`))
-  } else {
-    console.log(`Data ${event.data} is not supported`)
   }
 }
