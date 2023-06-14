f7fc8b504d02330253c46b53d5e3bc46a4db7f88
Andrejs Pavlovics <andrejs.pavlovics@gmail.com>
2023-06-14 16:04:57 +0200

    Complete LobbySessionSpec

--- zio/src/test/scala/lv/continuum/lobby/zio/LobbySessionSpec.scala
+++ zio/src/test/scala/lv/continuum/lobby/zio/LobbySessionSpec.scala
@@ -1,6 +1,6 @@
 package lv.continuum.lobby.zio
 
-import lv.continuum.lobby.protocol.Protocol.{Out, Password, UserType, Username}
+import lv.continuum.lobby.protocol.Protocol.{Out, Password, PushOut, UserType, Username}
 import lv.continuum.lobby.protocol.Protocol.UserType.{Admin, User}
 import lv.continuum.lobby.protocol.TestData
 import lv.continuum.lobby.zio.LobbySession.Env
@@ -75,22 +75,45 @@ object LobbySessionSpec extends ZIOSpecDefault with TestData {
         test("subscribe and unsubscribe via TableIn messages")(
           processSubscribeAndUnsubscribeWhenLoggedIn()
         ),
+        test("handle AddTable, UpdateTable and RemoveTable messages and notify subscribers")(
+          processSubscribeAndUnsubscribeWhenLoggedIn(mockSubscriber =>
+            for {
+              outAddTable      <- LobbySession.process(Right(addTable._2), mockSubscriber)
+              pushOutsAddTable <- mockSubscriber.pushOuts
+
+              outUpdateTable      <- LobbySession.process(Right(updateTable._2), mockSubscriber)
+              pushOutsUpdateTable <- mockSubscriber.pushOuts
+
+              outRemoveTable      <- LobbySession.process(Right(removeTable._2), mockSubscriber)
+              pushOutsRemoveTable <- mockSubscriber.pushOuts
+            } yield Some(
+              List(
+                assert(outAddTable)(isNone),
+                assert(pushOutsAddTable)(equalTo(Vector(tableAdded._2))),
+                assert(outUpdateTable)(isNone),
+                assert(pushOutsUpdateTable)(hasLast(equalTo(tableUpdated._2))),
+                assert(outRemoveTable)(isNone),
+                assert(pushOutsRemoveTable)(hasLast(equalTo(tableRemoved._2))),
+              ).reduce(_ && _)
+            ),
+          )
+        ),
         test("handle failure upon AddTable message")(
-          processSubscribeAndUnsubscribeWhenLoggedIn(
+          processSubscribeAndUnsubscribeWhenLoggedIn(mockSubscriber =>
             for {
               out <- LobbySession.process(Right(addTableInvalid), stubSubscriber)
             } yield Some(assert(out)(isSome(equalTo(tableAddFailed._2))))
           )
         ),
         test("handle failure upon UpdateTable message")(
-          processSubscribeAndUnsubscribeWhenLoggedIn(
+          processSubscribeAndUnsubscribeWhenLoggedIn(mockSubscriber =>
             for {
               out <- LobbySession.process(Right(updateTableInvalid), stubSubscriber)
             } yield Some(assert(out)(isSome(equalTo(tableUpdateFailed._2))))
           )
         ),
         test("handle failure upon RemoveTable message")(
-          processSubscribeAndUnsubscribeWhenLoggedIn(
+          processSubscribeAndUnsubscribeWhenLoggedIn(mockSubscriber =>
             for {
               out <- LobbySession.process(Right(removeTableInvalid), stubSubscriber)
             } yield Some(assert(out)(isSome(equalTo(tableRemoveFailed._2))))
@@ -105,14 +128,27 @@ object LobbySessionSpec extends ZIOSpecDefault with TestData {
       ),
     )
 
-  private val stubSubscriber = new Subscriber {
+  private val stubSubscriber: Subscriber = (pushOut: PushOut) => ZIO.unit
 
-    override def send(out: Out): Task[Unit] = ZIO.unit
+  class MockSubscriber(
+    pushOutsRef: Ref[Vector[PushOut]]
+  ) extends Subscriber {
+
+    override def send(pushOut: PushOut): Task[Unit] =
+      pushOutsRef.update(_ :+ pushOut)
+
+    def pushOuts: UIO[Vector[PushOut]] = pushOutsRef.get
+  }
+  object MockSubscriber {
+
+    def make: UIO[MockSubscriber] =
+      for {
+        pushOutsRef <- Ref.make(Vector.empty[PushOut])
+      } yield MockSubscriber(pushOutsRef)
   }
 
-  private def stubAuthenticator(userType: Option[UserType]): ULayer[Authenticator] = {
+  private def stubAuthenticator(userType: Option[UserType]): ULayer[Authenticator] =
     ZLayer.succeed { (username: Username, password: Password) => ZIO.succeed(userType) }
-  }
 
   private val processLogin: ZIO[Env, Nothing, Option[Out]] =
     LobbySession.process(Right(login._2), stubSubscriber)
@@ -124,19 +160,20 @@ object LobbySessionSpec extends ZIOSpecDefault with TestData {
     } yield assert(out)(isSome(equalTo(pong._2)))
 
   private def processSubscribeAndUnsubscribeWhenLoggedIn(
-    assertWhenSubscribed: ZIO[Env, Nothing, Option[TestResult]] = ZIO.succeed(None),
+    assertWhenSubscribed: MockSubscriber => ZIO[Env, Nothing, Option[TestResult]] = _ => ZIO.succeed(None),
   ): ZIO[Env, Nothing, TestResult] =
     for {
       _                           <- processLogin
-      subscribeOut                <- LobbySession.process(Right(subscribeTables._2), stubSubscriber)
+      mockSubscriber              <- MockSubscriber.make
+      subscribeOut                <- LobbySession.process(Right(subscribeTables._2), mockSubscriber)
       subscribersWhenSubscribed   <- ZIO.serviceWithZIO[SubscribersHolder](_.subscribers)
-      testResultWhenSubscribed    <- assertWhenSubscribed
-      unsubscribeOut              <- LobbySession.process(Right(unsubscribeTables._2), stubSubscriber)
+      testResultWhenSubscribed    <- assertWhenSubscribed(mockSubscriber)
+      unsubscribeOut              <- LobbySession.process(Right(unsubscribeTables._2), mockSubscriber)
       subscribersWhenUnsubscribed <- ZIO.serviceWithZIO[SubscribersHolder](_.subscribers)
     } yield {
       (List(
         assert(subscribeOut)(isSome(equalTo(tableList._2))),
-        assert(subscribersWhenSubscribed)(hasSameElements(Set(stubSubscriber))),
+        assert(subscribersWhenSubscribed)(equalTo(Set(mockSubscriber))),
         assert(unsubscribeOut)(isNone),
         assert(subscribersWhenUnsubscribed)(isEmpty),
       ) ++ testResultWhenSubscribed).reduce(_ && _)
